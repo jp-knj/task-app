65cd3c67683ac9e0cf2c1c8b9b9ed3dd71f1de3d
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-02-14 17:04:41 +0900

    :sparkles: Create a Todo Type, Model, Api Controller, Route and Server
    t

---
+++ server/src/routes/index.ts
@@ -0,0 +1,10 @@
+import {Router} from "express"
+import {getTodos, addTodo} from "../controllers/todos"
+
+const router: Router = Router()
+
+router.get("/todos", getTodos)
+
+router.post("/add-todo", addTodo)
+
+export default router
