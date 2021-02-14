b51bf1e38e4daf6be1865d1f21b2e1de47360349
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-02-14 17:54:48 +0900

    Forget add and commit
    t

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
+// Components
+import TodoItem from './components/TodoItem'
+import AddTodo from './components/AddTodo'
+
 const App: React.FC = () => {
 
 return (
     <main className='App'>
       <h1>My Todos</h1>
+        <AddTodo/>
+        <TodoItem/>
     </main>
   )
 }
